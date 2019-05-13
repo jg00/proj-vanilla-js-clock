d85cad882d7c88bde47f45a506ed631200606ab1
Jack Gamboa <jgamboa929@gmail.com>
2019-05-13 08:46:34 -0500

    CSS/JS clock complete.

---
+++ index.js
@@ -0,0 +1,24 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+const offsetHandDegrees = 90;
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + offsetHandDegrees;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minutesDegrees = (mins / 60) * 360 + offsetHandDegrees;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hour = now.getHours();
+  console.log(hour);
+  const hoursDegrees = (hour / 12) * 360 + offsetHandDegrees;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
